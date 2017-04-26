d5a3368d44d85341fee1d4ce29ff143c9f7117ef
Kai Mindermann <kai.mindi@gmx.de>
2017-04-26 22:01:54 +0200

    initial (almost still copy from EncryptedString) EncryptedFile

---
+++ src/main/java/com/cryptoexamples/java/ExampleEncryptedFile.java
@@ -0,0 +1,41 @@
+package com.cryptoexamples.java;
+
+import java.security.GeneralSecurityException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// TODO check if KeyStore should be used
+
+/**
+ * Object oriented example for encryption and decryption of a string;
+ * Including
+ * - random password generation,
+ * - random salt generation,
+ * - key derivation using PBKDF2 HMAC SHA-256,
+ * - AES-256 authenticated encryption using GCM
+ * - BASE64-encoding for the byte-arrays
+ * - exception handling
+ */
+public class ExampleEncryptedFile {
+  private static final Logger LOGGER = Logger.getLogger(ExampleEncryptedFile.class.getName());
+
+  public static void main(String[] args) {
+    String plainText = "Text that is going to be sent over an insecure channel and must be encrypted at all costs!";
+
+    try {
+      // GENERATE a password (if a password exists, use that).
+      String password = EncryptedFile.generatePassword(32);
+
+      // ENCRYPTION
+      EncryptedFile encryptedFile = new EncryptedFile().encrypt(plainText, password);
+
+      // DECRYPTION
+      String decryptedCipherText = encryptedFile.decrypt(password);
+
+      LOGGER.log(Level.INFO, () -> String.format("Decrypted and original plain text are the same: %s", decryptedCipherText.compareTo(plainText) == 0 ? "true" : "false"));
+    } catch (GeneralSecurityException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+    }
+  }
+
+}
