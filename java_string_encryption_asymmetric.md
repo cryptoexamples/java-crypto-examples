4917d3ac7d5c14a2012ef8b6b6ee1adc9c362abc
Kai Mindermann M.Sc <kai.mindi@gmx.de>
2018-04-28 19:29:08 +0200

    adds asymmetric string encryption example (#5)

---
+++ java_string_encryption_asymmetric.md
@@ -0,0 +1,41 @@
+---
+title: Asymmetric String Encryption using Java JDK
+keywords: sample
+summary: "Asymmetric String Encryption in Java"
+permalink: java_asymmetric_string_encryption.html
+folder: Java JDK
+references: [
+    # Place a list of references used to create and/or understand this example.
+    {
+        url: "https://docs.oracle.com/javase/10/docs/api/javax/crypto/Cipher.html",
+        description: "Java JDK Ciper"
+    }
+]
+authors: [
+    {
+        name: "Kai Mindermann",
+        url: "https://github.com/kmindi"
+    }
+]
+# List all reviewers that reviewed this version of the example. When the example is updated all old reviews
+# must be removed from the list below and the code has to be reviewed again. The complete review process
+# is documented in the main repository of CryptoExamples
+current_reviews: [
+
+]
+# Indicates when this example was last updated/created. Reviews don't change this.
+last_updated: "2018-04-28"
+tags: [Java, RSA, Asymmetric, String, Encryption]
+---
+
+## Use cases
+
+- 
+
+## Sample Code for Java based asymmetric encryption 
+
+```java
+{% include_relative src/main/java/com/cryptoexamples/java/ExampleAsymmetricStringEncryptionInOneMethod.java %}
+```
+
+{% include links.html %}
---
+++ src/main/java/com/cryptoexamples/java/ExampleAsymmetricStringEncryptionInOneMethod.java
@@ -0,0 +1,56 @@
+package com.cryptoexamples.java;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.InvalidParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * All in one example for asymmetric encryption and decryption of a string in one method.
+ * - Generation of public and private RSA 4096 bit keypair
+ * - AES-256 authenticated encryption using GCM
+ * - BASE64-encoding as representation for the byte-arrays
+ * - UTF-8 encoding of String
+ * - Exception handling
+ */
+public class ExampleAsymmetricStringEncryptionInOneMethod {
+  private static final Logger LOGGER = Logger.getLogger(ExampleAsymmetricStringEncryptionInOneMethod.class.getName());
+
+  public static void main(String[] args) {
+    String plainText = "Text that is going to be sent over an insecure channel and must be encrypted at all costs!";
+    try {
+
+      // GENERATE NEW KEYPAIR
+      KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+      /* @see https://www.keylength.com/ */
+      keyPairGenerator.initialize(4096);
+      KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+      // ENCRYPTION
+      Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+      cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+
+      byte[] byteCipher = cipher.doFinal(plainText.getBytes());
+
+      // CONVERSION of raw bytes to BASE64 representation
+      String cipherText = new String(Base64.getEncoder().encode(byteCipher));
+
+      // DECRYPTION
+      cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+      byte[] decryptedCipher = cipher.doFinal(Base64.getDecoder().decode(cipherText));
+      String decryptedCipherText = new String(decryptedCipher);
+
+      LOGGER.log(Level.INFO, () -> String.format("Decrypted and original plain text are the same: %b", decryptedCipherText.compareTo(plainText) == 0));
+    } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidParameterException  e) {
+      LOGGER.log(Level.SEVERE, e.getMessage(), e);
+    }
+  }
+}
